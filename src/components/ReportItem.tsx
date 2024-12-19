c7a0ea999ba5383f82fc15dc2b1a93f62628cd06
Sagar Kori <sagarkoriup11@gmail.com>
2024-12-19 14:59:31 +0530

    Enhanced UI and removed CORS error

--- src/components/ReportItem.tsx
+++ src/components/ReportItem.tsx
@@ -39,8 +39,10 @@ const ReportItemPopup: React.FC = () => {
         console.log("For each image func done", images);
 
         try {
+            const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
             setloading(true)
-            const response = await fetch("http://localhost:3000/api/items", {
+            const uploadUrl = `${BASE_URL}/api/items`;
+            const response = await fetch(uploadUrl, {
                 method: "POST",
                 body: formData,
             });
