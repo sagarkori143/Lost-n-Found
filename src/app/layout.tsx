5f53d8017692e364c7558ecb5f7e3cb308d86e65
Sagar Kori <sagarkoriup11@gmail.com>
2025-01-01 12:05:29 +0530

    Added analytics plugin for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Toaster position="bottom-right" />
         {children}
+        <Analytics/> 
       </body>
     </html>
   );
